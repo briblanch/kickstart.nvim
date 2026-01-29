31f94cad646eaca2bdc0f0d128b07d55f9cb5178
Brian Blanchard <brian.blanchard@workiva.com>
2026-01-29 15:35:45 -0700

    More config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview' },
+    build = 'cd app && npm install',
+    ft = { 'markdown' },
+  },
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', '<cmd>vertical Git<CR>')
+    end,
+  },
+}
